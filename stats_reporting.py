c7b2318559b949f1801a2d9b00da57d7f9de6f31
Jesse Zlata <jesse.zlata@warbyparker.com>
2015-03-19 17:16:58 -0400

    try with out a thread first

--- stats_reporting.py
+++ stats_reporting.py
@@ -1,5 +1,4 @@
 from ultrasound import *
-import threading
 import socket
 
 ultrasound = Ultrasound()
@@ -8,20 +7,10 @@ UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
 
 
-def ultrasound_stuff():
-    while True:
-        message = ultrasound.checkForHuman()
-        print message
-        sock = socket.socket(socket.AF_INET, # Internet
-                             socket.SOCK_DGRAM) # UDP
-        sock.sendto(message, (UDP_IP, UDP_PORT))
-
-threads = []
-range_thread = threading.Thread(target=ultrasound_stuff)
-
-range_thread.daemon = True
-
-threads.append(range_thread)
-
-range_thread.start()
+while True:
+    message = ultrasound.checkForHuman()
+    print message
+    sock = socket.socket(socket.AF_INET, # Internet
+                         socket.SOCK_DGRAM) # UDP
+    sock.sendto(message, (UDP_IP, UDP_PORT))
 
